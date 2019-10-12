968be24a4b38dfab4e11e75bacc301ae7ea2a4f2
BRE15150326 <C-brewer@hotmail.co.uk>
2019-10-12 16:55:26 +0100

    Player Horizontal Movement
    The player can move side to side along the Z axis.

---
+++ ArrowNinja/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float playerSpeed = 5.0f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(0, 0, Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
+    }
+}
