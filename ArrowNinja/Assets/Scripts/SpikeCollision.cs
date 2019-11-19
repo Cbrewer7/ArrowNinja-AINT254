483ffb56bc855e4142b6bfc912126af791ddaed9
BRE15150326 <C-brewer@hotmail.co.uk>
2019-11-19 14:21:07 +0000

    Quick Level
    Changed the camera to follow the arrow marker instead of player and made a quick level run for interaction testing.

--- ArrowNinja/Assets/Scripts/SpikeCollision.cs
+++ ArrowNinja/Assets/Scripts/SpikeCollision.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class SpikeCollision : MonoBehaviour
 {
-    // Update is called once per frame
+    int lives = 3;
+
     void Update()
     {
-        
+        //Display Game Over
+        if (lives <= 0)
+        {
+
+        }
     }
 
     //Spike collision
@@ -15,6 +20,7 @@ public class SpikeCollision : MonoBehaviour
     {
         if (collision.gameObject.tag == "SpikeObject")
         {
+            lives--;
             Destroy(gameObject);
         }
     }
