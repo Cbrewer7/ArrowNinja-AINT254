8add7f96cd9751fb1d16b43a86e358a7f1871035
BRE15150326 <C-brewer@hotmail.co.uk>
2019-11-19 12:49:10 +0000

    Wall Spike/Camera Follow/Platform
    There is a prefab for a basic wall spike that only destroys the player at the moment. The camera now follows the player across the level. There is now a prefab for a basic platform the player can land on.

---
+++ ArrowNinja/Assets/Scripts/SpikeCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCollision : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Spike collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SpikeObject")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
